3670c29984d3dece7d2350821b889b4a5fcd3558
kiranna23 <bond78130@gmail.com>
2024-02-13 19:51:21 +0300

    two commit

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
