8500811c4c26bcfe41c893fc490384cb7116249e
kiranna23 <bond78130@gmail.com>
2024-02-21 23:05:21 +0300

    add lesson 3

---
+++ lesson3/ArrayInt.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    class ArrayInt
+    {
+        int[] numbers;
+
+        public ArrayInt(int size)
+        {
+            numbers = new int[size];    
+        }
+
+        public ArrayInt(int[] array)
+        {
+            numbers = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                numbers[i] = array[i];  
+            }
+        }
+        public void CopyArray(int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public int[] ReturnArray()
+        {
+            return numbers; 
+        }
+
+        public void InputRandom()
+        {
+            Random rnd = new Random();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(10);
+            }
+        }
+
+        public void InputConsole()
+        {
+            Console.Write("Enter an array: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.Read());
+            }
+        }
+
+        public void Output()
+        {
+            Console.Write("array of numbers: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();    
+        }
+
+        public int NumberInArray(int number)
+        {
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public bool DeleteNumber(int number)
+        {
+            int j = 0;
+            if (NumberInArray(number)>0)
+            {
+                int[] NewNumbers = new int[numbers.Length - NumberInArray(number)]; 
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] != number)
+                    {
+                        NewNumbers[j] = numbers[i];
+                        j++;
+                    }
+                }
+                numbers = NewNumbers;   
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int max()
+        {
+            return numbers.Max();
+        }
+        
+        public int min()
+        {
+            return numbers.Min();   
+        }
+
+        public double average()
+        {
+            return numbers.Average();   
+        }
+
+        public void ChangeOnZero()
+        {
+            for (int i = 1; i < numbers.Length; i=i+2)
+            {
+                numbers[i] = 0;
+            }
+        }
+
+        public void SortArray()
+        {
+            int x = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        x = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = x;
+                    }
+                }
+            }
+        }
+
+        public int[] EvenNumber()
+        {
+            int size = 0;
+            for (int i =0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    size++; 
+                }
+            }
+            if (size > 0)
+            {
+                int[] number = new int[size];
+                int j = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        number[j] = numbers[i];
+                        j++;
+                    }
+                }
+                return number;  
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ lesson3/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayInt array = new ArrayInt(10); //exercise 0
+            array.InputRandom();
+            array.Output();
+            Console.Write("Enter a number to search in the array: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (array.NumberInArray(number) > 0)
+            {
+                Console.WriteLine("The number is included in the array");
+            }
+            else
+            {
+                Console.WriteLine("The number isn't included in the array");
+            }
+
+            array.InputRandom(); //exercise 1
+            array.Output();
+            Console.Write("Enter a number to delete in the array: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (array.DeleteNumber(number))
+            {
+                array.Output();
+            }
+            else
+            {
+                Console.WriteLine("The number isn't included in the array");
+            }
+
+            int size = 0; //exercise 2
+            Console.Write("Enter a size of the array: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            ArrayInt array1 = new ArrayInt(size);
+            array1.InputRandom();
+            array1.Output();
+            Console.WriteLine("Minimum: " + array1.min() + ", maximum: " + array1.max() + ", average: " + array1.average() + "\n");
+
+            ArrayInt FirstArray = new ArrayInt(5);  //exercise 3
+            ArrayInt SecondArray = new ArrayInt(5);
+            FirstArray.InputRandom();
+            SecondArray.InputRandom();
+            FirstArray.Output();
+            SecondArray.Output();
+            double average1 = FirstArray.average();
+            double average2 = SecondArray.average();
+            Console.WriteLine("The first average: " + average1 + ", the second average: " + average2);
+            if (average1 > average2)
+            {
+                Console.WriteLine("The average of the first array is more than the second.");
+            }
+            else if (average1 < average2)
+            {
+                Console.WriteLine("The average of the second array is more than the first.");
+            }
+            else
+            {
+                Console.WriteLine("The averages is equal.");
+            }
+
+            bool check = false;  //exercise 4
+            int n = 0;
+            Console.Write("Enter size (5, 10]: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                if (n > 5 && n <= 10)
+                {
+                    check = true;
+                }
+                else
+                {
+                    Console.Write("Enter the size again: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (!check);
+            array = new ArrayInt(n);
+            array.InputRandom();
+            array.Output();
+            int[] NewArray = array.EvenNumber();
+            if (NewArray.Length > 0)
+            {
+                ArrayInt array2 = new ArrayInt(NewArray);
+                Console.Write("An array of even elements of the first array - ");
+                array2.Output();
+            }
+            else
+            {
+                Console.WriteLine("Array hasn't even numbers");
+            }
+
+            array = new ArrayInt(10); //exercise 5
+            array.InputRandom();
+            array.Output();
+            array.ChangeOnZero();
+            Console.Write("An array with zeros under no even indexes - ");
+            array.Output();
+
+            ArrayStr names = new ArrayStr(); //exercise 6
+            names.Output();
+            names.SortArray();
+            Console.Write("Sorted names: ");
+            names.Output();
+
+            array = new ArrayInt(10);  //exercise 7
+            array.InputRandom();
+            array.Output();
+            array.SortArray();
+            Console.Write("Sorted ");
+            array.Output();
+
+            Matrix matrix = new Matrix(3, 4); //exercise 8
+            matrix.InputByHands();
+            matrix.OutputMatrix();
+            Matrix matrix1 = new Matrix(4, 3);
+            matrix1.InputByHands();
+            matrix1.OutputMatrix();
+            int[,] MyMatrix = matrix.Multiplication(matrix1);
+            Matrix matrix2 = new Matrix(MyMatrix);
+            Console.WriteLine("The matrix after multiplication\n");
+            matrix2.OutputMatrix();
+
+            matrix = new Matrix(3, 3); //exercise 9
+            matrix.InputRandom();
+            matrix.OutputMatrix();
+            Console.WriteLine("Sum of matrix is " + matrix.Sum() + "\n\n");
+
+            matrix = new Matrix(4, 4); //exercise 10
+            matrix.InputRandom();
+            matrix.OutputMatrix();
+            matrix.Diagonal();
+
+            matrix.OutputMatrix();  //exercise 11
+            matrix.SortMatrix();
+            Console.WriteLine("Sorted matrix");
+            matrix.OutputMatrix();
+        }
+    }
+}
